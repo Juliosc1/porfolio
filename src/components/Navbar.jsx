fde48493ee2ff7815b4216f3b45bf8ba1f1e61eb
Juliosc1 <julio.siklander@gmail.com>
2022-05-25 15:22:11 +0200

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { faBars } from 'react-icons'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.png';
 
 function Navbar() {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -19,13 +22,13 @@ function Navbar() {
             </ul>
 
         {/*hamburger*/}
-        <div className='md: hidden z-10'>
-            <faBars />
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
         
         {/*mobile menu*/}
-        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <ul className={!nav ? 'hidden ' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4x1'>Home</li>
             <li className='py-6 text-4x1'>About</li>
             <li className='py-6 text-4x1'>Skills</li>
