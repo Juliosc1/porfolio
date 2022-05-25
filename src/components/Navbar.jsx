84f2121bcb2356d33d9745648d5362330657dd01
Juliosc1 <julio.siklander@gmail.com>
2022-05-25 18:04:54 +0200

    Add contact + scrollbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/logo.png';
+import {Link} from 'react-scroll'
 
 function Navbar() {
     const [nav, setNav] = useState(false)
@@ -16,11 +17,31 @@ function Navbar() {
 
         {/*menu*/}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                <Link to="home" smooth={true} duration={500}>
+                Home
+                </Link>
+                </li>
+                <li>
+                <Link to="about" smooth={true} duration={500}>
+                About
+                </Link>
+                </li>
+                <li>
+                <Link to="skills" smooth={true} duration={500}>
+                Skills
+                </Link>
+                </li>
+                <li>
+                <Link to="work" smooth={true} duration={500}>
+                Work
+                </Link>
+                </li>
+                <li>
+                <Link to="contact" smooth={true} duration={500}>
+                Contact
+                </Link>
+                </li>
             </ul>
 
         {/*hamburger*/}
@@ -31,11 +52,31 @@ function Navbar() {
         
         {/*mobile menu*/}
         <ul className={!nav ? 'hidden ' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4x1'>Home</li>
-            <li className='py-6 text-4x1'>About</li>
-            <li className='py-6 text-4x1'>Skills</li>
-            <li className='py-6 text-4x1'>Work</li>
-            <li className='py-6 text-4x1'>Contact</li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                Home
+            </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                About
+            </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                Skills
+            </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                Work
+            </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                Contact
+            </Link>    
+            </li>
         </ul>
 
         
