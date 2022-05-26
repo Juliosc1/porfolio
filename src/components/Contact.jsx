b5c668c1d6df2c7fb64fa18808b485855328c841
Juliosc1 <julio.siklander@gmail.com>
2022-05-26 19:29:49 +0200

    Update font-family

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xnqwwdyz");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action='' className='flex flex-col max-w-[600px] w-full'>
+        <form onSubmit={handleSubmit} action='' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an eamil - julio.siklander@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name='message' rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <label htmlFor="email">
+            Email Address
+            </label>
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" id="email" placeholder='email' name='email' />
+            <ValidationError 
+            prefix="Email" 
+            field="email"
+            errors={state.errors}
+          />
+            <textarea className='bg-[#ccd6f6] p-2' id="message" name='message' rows="10" placeholder='Message'></textarea>
+            <ValidationError 
+            prefix="Message" 
+            field="message"
+            errors={state.errors}
+            />
+            <button type="submit" disabled={state.submitting} className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
   )
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -17,16 +17,16 @@ const Work = () => {
 
                 {/*Grid Item*/}
                 <div style={{backgroundImage: `url(${PortfolioImg})`}} 
-                className={'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'}>
+                className={style.item}>
                    
                     {/*Hover effects*/}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
+                    <div className={style.one}>
+                        <span className={style.portfolioText}>
                             Portfolio
                         </span>
-                        <div className='pt-8 text-center'>
+                        <div className={style.btnContainer}>
                             <a href="https://porfolio-theta-ten.vercel.app/" target="_blank" rel="noreferrer noopener">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                <button className={style.btnDemo}>Demo</button>
                             </a>
                             <a href='https://github.com/Juliosc1/porfolio' target="_blank" rel="noreferrer noopener">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
